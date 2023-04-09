dfd1acb1dc62275a0db7cdd73c46bc982ff03766
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-09 23:48:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Escape Sequence
+
+#It adds a slash '\', meaning it tells python that everything after that is a comment
+# \t = adds a tab or indent
+# \n = makes a new line
+
+weather = "It\'s \"kind of\" sunny" #Make sure to add the \ infront of all quotation marks
+
+print(weather)
+
+weather_1 = "\t It\'s \"kind of\" sunny \n hope you have a good day!" #\t = tab or indent, \n = new line
+
+print(weather_1) 
